bb86671f1860f236c0488fe59ad310e614bfa622
ViktorKil <presariohp13@gmail.com>
2025-01-14 15:51:31 +0700

    верстка блоков gallery и customers

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,16 @@
-console.log('Hello JS')
+import fslightbox from 'fslightbox'
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'
+// import styles bundle
+import 'swiper/css/bundle'
+
+const swiper = new Swiper('.customers-slider', {
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
